2ddfbd0bb33a649c57acdc276f510376ad9d73ed
Cameron-Sconza <cameron.sconza@onshoreoutsourcing.com>
2017-07-17 16:06:26 -0400

    Removed obsolute project, and Replaced with New Project

---
+++ ORA/ORA/Controllers/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORA.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        // GET: MainMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ORA/ORA/Properties/AssemblyInfo.cs
+++ ORA/ORA/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77e7567e-8624-46e3-9669-0d5dc00a2730")]
+[assembly: Guid("253fd77b-3e11-45e7-acec-84733699be38")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
