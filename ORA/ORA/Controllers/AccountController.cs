4c61593583f0b65c33eb885f1ef737d014ab2ae8
Eric Crowe <ericacrowe24@gmail.com>
2017-07-18 10:32:51 -0400

    Merged master into eric

---
+++ ORA/ORA/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORA.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string replaceWithAppropriateValue)
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Register(string replaceWithAppropriateValue)
+        {
+            return View();
+        }
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+    }
+}
