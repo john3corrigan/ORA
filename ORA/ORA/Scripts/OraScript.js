05d1af456b8c334fc5d6a0edeb4731ff8b1c7241
Michael Wilson <michael.wilson@onshoreoutsourcing.com>
2017-08-15 08:15:12 -0400

    atempted highlighted tag

--- ORA/ORA/Scripts/OraScript.js
+++ ORA/ORA/Scripts/OraScript.js
@@ -1,1 +1,11 @@
-﻿
+﻿function tabActive(btnName) {
+    var i, buttonName;
+
+    buttonName = document.getElementsByClassName("dropbtn");
+    for (i = 0; i < buttonName.length; i++) {
+        buttonName[i].className = buttonName[i].className.replace(" active", "");
+    }
+
+    document.getElementById(btnName).className += " active";
+
+}
